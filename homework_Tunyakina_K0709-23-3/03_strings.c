76356b3d3d687ef562a507570f00e83f19ceabfb
Ksenix-CopLLeRe <tunyakina.ksenia@gmail.com>
2025-03-05 14:20:50 +0300

    commit14

--- homework_Tunyakina_K0709-23-3/03_strings.c
+++ homework_Tunyakina_K0709-23-3/03_strings.c
@@ -9,7 +9,7 @@ void task4();
 void task5();
 
 int main() {
-    printf("Домашнее задание №5 Тюнякиной Ксении, группа К0709-23/3\n");
+    printf("Задачи на строки Тюнякиной Ксении, группа К0709-23/3\n");
     printf("Чтобы завершить проверку, введите 0.\n");
     int number = 1;
     while (number != 0) {
@@ -39,9 +39,34 @@ int main() {
     }
 }
 
-void task1() {}
+void task1() {
+    char *spaceships[100];
+    int n;
 
-// ???
+    printf("Введите количество кораблей: ");
+    scanf("%d", &n);
+    printf("Введите названия кораблей: ");
+    for (int i = 0; i < n; i++) {
+        spaceships[i] = (char*) malloc(sizeof(char*) * n);
+        scanf("%50s", spaceships[i]);
+    }
+
+    int find = 0;
+    for (int i = 0; i < n; i++) {
+        if (strncmp(spaceships[i], "SS", 2) == 0 || strncmp(spaceships[i], "ss", 2) == 0) {
+            printf("%s\n", spaceships[i]);
+            find = 1;
+            break;
+        }
+    }
+
+    if (find == 0) {
+        printf("Не найдено.\n");
+    }
+
+    free(*spaceships);
+}
+
 void task2() {
     char signal[101];
     printf("Введите зашифрованное сообщение: ");
@@ -70,7 +95,16 @@ void task3() {
     }
 }
 
-void task4() {}
+void task4() {
+    char code[100];
+    printf("Введите код запуска: ");
+    scanf("%100s", code);
+    if (strcasecmp(code, "launch-2025") == 0 || strcasecmp(code, "LAUNCH-2025") == 0) {
+        printf("Код принят.\n");
+    } else {
+        printf("Ошибка: неверный код!\n");
+    }
+}
 
 void task5() {
     char report[200];
